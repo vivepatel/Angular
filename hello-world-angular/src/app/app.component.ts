991358d09ecf1d3394cb75f340591be82be97635
Vivekpatel <vp20032020@gmail.com>
2025-03-06 12:35:06 +0530

    [Vivek] Added UC6

--- hello-world-angular/src/app/app.component.ts
+++ hello-world-angular/src/app/app.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
+import { NgIf } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [FormsModule], 
+  imports: [FormsModule, NgIf], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   userName: string = ""; // Stores user input
   message: string = "Hello from BridgeLabz";
-  logoUrl: string = "hello-world-angular\src\assets\BL_logo_square_png.png"; // Local image path
+  logoUrl: string = "assets/bridgelabz-logo.png"; // Local image path
+  errorMessage: string = ""; // Stores validation error
 
-  // Method to open BridgeLabz website in a new tab
+ 
+  validateUserName() {
+    const regex = /^[A-Z][a-zA-Z]{2,}$/; // Must start with uppercase & be at least 3 characters
+    if (!regex.test(this.userName)) {
+      this.errorMessage = "Name must start with a capital letter and have at least 3 letters!";
+    } else {
+      this.errorMessage = ""; // Clear error if valid
+    }
+  }
+
+  
   openBridgeLabzSite() {
     window.open("https://www.bridgelabz.com", "_blank");
   }
