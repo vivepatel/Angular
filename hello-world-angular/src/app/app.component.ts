d9dbbfbb6635232b77cf7bfa2ba81b30047662d2
Vivekpatel <vp20032020@gmail.com>
2025-03-06 12:18:40 +0530

    [Vivek] Added UC5

--- hello-world-angular/src/app/app.component.ts
+++ hello-world-angular/src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-root',
+  standalone: true, 
+  imports: [FormsModule], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message: string = "Hello from BridgeLabz";  
+  userName: string = ""; // Stores user input
+  message: string = "Hello from BridgeLabz";
   logoUrl: string = "hello-world-angular\src\assets\BL_logo_square_png.png"; // Local image path
 
-  // Method to launch BridgeLabz site in a new tab
+  // Method to open BridgeLabz website in a new tab
   openBridgeLabzSite() {
     window.open("https://www.bridgelabz.com", "_blank");
   }
